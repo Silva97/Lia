9fee93626db25d9eac95d70577602d98fc82d1f5
Luiz Felipe <felipe.silva337@yahoo.com>
2020-04-29 20:07:28 -0300

    [fix] The tree is independent now!

--- include/tree.h
+++ include/tree.h
@@ -1,12 +1,14 @@
-#ifndef _LIA_TREE_H
-#define _LIA_TREE_H
+#ifndef _TREE_H
+#define _TREE_H
 
+/** Macro to expand basic struct elements of a tree */
 #define EXTENDS_TREE(strname) \
   struct strname *left;        \
   struct strname *right;      \
   unsigned long int hashname;  \
   char *name
 
+/** A basic tree */
 typedef struct tree {
   EXTENDS_TREE(tree);
 } tree_t;
@@ -15,4 +17,4 @@ void *tree_insert(void *tree, unsigned int size, unsigned long int hashname);
 void *tree_find(void *tree, unsigned long int hashname);
 void tree_free(void *tree);
 
-#endif /* _LIA_TREE_H */
+#endif /* _TREE_H */

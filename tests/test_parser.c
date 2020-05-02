3a3a47e571f886d3888cd59c6eb557d15c77999d
Luiz Felipe <felipe.silva337@yahoo.com>
2020-05-01 21:43:06 -0300

    Test for new meta-instruction

---
+++ tests/test_parser.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lia/lia.h"
+#include "metric.h"
+
+#define TESTNEW "tests/new.lia"
+
+static void cmd_print(cmd_t *cmd)
+{
+  printf("%s ", cmd->name);
+  
+  for (int i = 0; i < CMD_ARGC; i++)
+    printf("%c:%c ", cmd->args[i].name, cmd->args[i].type);
+  
+  printf("= \"%s\"\n", cmd->body);
+}
+
+static void tree_print(cmd_t *tree, unsigned int level)
+{
+  const int lv = 4;
+  if (tree->right)
+    tree_print(tree->right, level + lv);
+
+  
+  if (level > lv) {
+    printf("%*c+", level - lv + 2, '\0');
+    for (int i = 0; i < lv - 1; i++)
+      putchar('-');
+    
+    putchar(' ');
+  } else if (level == lv) {
+    fputs(" +--- ", stdout);
+  } else {
+    fputs("+ ", stdout);
+  }
+
+  cmd_print(tree);
+  
+  if (tree->left)
+    tree_print(tree->left, level + lv);
+}
+
+
+test_t test_new(void)
+{
+  lia_t *lia = calloc(1, sizeof *lia);
+  FILE *input = fopen(TESTNEW, "r");
+  lia->tklist = lia_lexer(TESTNEW, input);
+
+  int err = lia_parser(TESTNEW, lia);
+  tree_print(lia->cmdtree, 0);
+
+  if (err != 3) {
+    METRIC_TEST_FAIL("Error in parsing the code");
+  }
+
+  METRIC_TEST_OK("Three errors expected");
+}
+
+int main(void)
+{
+  METRIC_TEST(test_new);
+
+  METRIC_TEST_END();
+  return metric_count_tests_fail;
+}

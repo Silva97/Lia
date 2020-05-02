07faada5e43111d34e9ec37f17598b991385fbae
Luiz Felipe <felipe.silva337@yahoo.com>
2020-05-01 21:35:54 -0300

    Writes main header of the Lia

---
+++ include/lia/lia.h
@@ -0,0 +1,11 @@
+#ifndef _LIA_H
+#define _LIA_H
+
+#include "lia/types.h"
+#include "lia/error.h"
+#include "lia/lexer.h"
+#include "lia/cmd.h"
+#include "lia/parser.h"
+#include "lia/procedure.h"
+
+#endif /* _LIA_H */

ea92657c044efdf5e4c894c8aeb6b3522758c90d
Luiz Felipe <felipe.silva337@yahoo.com>
2020-05-05 20:21:19 -0300

    Add tests for modules 'lia' and 'var'.

---
+++ test_modules.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+tdir="tests/modules"
+
+function assert_equ() {
+  if [ "$1" != "$2" ]; then
+    echo -e "${FUNCNAME[1]}() - Assertion fail: \"$1\" == \"$2\""
+    return 1
+  fi
+
+  return 0
+}
+
+
+function test_lia() {
+  local expects="Abcd"
+  local output=$(./lia "$tdir/test_lia.lia" -o- | ases)
+
+  assert_equ "$expects" "$output"
+  return
+}
+
+function test_var() {
+  local expects="abcdef"
+  local output=$(./lia "$tdir/test_var.lia" -o- | ases)
+
+  assert_equ "$expects" "$output"
+  return
+}
+
+
+test_lia || exit 1
+test_var || exit 2
+
+exit 0

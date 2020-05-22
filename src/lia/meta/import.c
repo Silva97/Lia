3317a2157487970a23f8b282be00980d1b794ab8
Luiz Felipe <felipe.silva337@yahoo.com>
2020-05-21 23:34:25 -0300

    Update meta-import and meta-require.
    - Now replaces '$' to target name, if a target been defined.

--- src/lia/meta/import.c
+++ src/lia/meta/import.c
@@ -2,17 +2,19 @@
  * @file    import.c
  * @author  Luiz Felipe (felipe.silva337@yahoo.com)
  * @brief   The meta-import parser.
- * @version 0.1
+ * @version 0.2
  * @date    2020-05-08
  * 
  * @copyright Copyright (c) 2020 Luiz Felipe
  */
 #include <stdio.h>
+#include <string.h>
 #include "lia/lia.h"
 #include "filepath.h"
 
 token_t *meta_import(KEY_ARGS)
 {
+  char name[TKMAX + 32];
   token_t *first = tk;
 
   while (tk) {
@@ -24,15 +26,20 @@ token_t *meta_import(KEY_ARGS)
       return NULL;
     }
 
-    FILE *input = pfind(tk->text, "r", lia->pathlist);
+    if (lia->target)
+      chrrep(name, tk->text, '$', lia->target->name);
+    else
+      strcpy(name, tk->text);
+
+    FILE *input = pfind(name, "r", lia->pathlist);
 
     if ( !input ) {
       lia_error(file->filename, first->line, first->column,
-        "Module \"%s\" not found.", tk->text);
+        "Module \"%s\" not found.", name);
       return NULL;
     }
 
-    lia_process(tk->text, input, lia);
+    lia_process(name, input, lia);
     fclose(input);
 
     tk = metanext(tk);
--- src/lia/meta/require.c
+++ src/lia/meta/require.c
@@ -2,18 +2,21 @@
  * @file    require.c
  * @author  Luiz Felipe (felipe.silva337@yahoo.com)
  * @brief   The meta-require parser.
- * @version 0.1
+ * @version 0.2
  * @date    2020-05-13
  * 
  * @copyright Copyright (c) 2020 Luiz Felipe
  */
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "lia/lia.h"
 #include "tree.h"
 
 token_t *meta_require(KEY_ARGS)
 {
+  char name[TKMAX + 32];
+
   while (tk) {
     tk = metanext(tk);
 
@@ -24,9 +27,14 @@ token_t *meta_require(KEY_ARGS)
       return NULL;
     }
 
-    if ( !tree_find(lia->imptree, hash(tk->text)) ) {
+    if (lia->target)
+      chrrep(name, tk->text, '$', lia->target->name);
+    else
+      strcpy(name, tk->text);
+
+    if ( !tree_find(lia->imptree, hash(name)) ) {
       lia_error(file->filename, tk->line, tk->column,
-        "Required module '%s' not imported yet.", tk->text);
+        "Required module '%s' not imported yet.", name);
       file->stop = true;
       return NULL;
     }
